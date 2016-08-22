88ea91585d4c054f0feb0c2719fa25e92da57e8f
Matt.C <linkinpark761102@bwwtaiwan.com>
2016-08-22 22:03:59 +0800

    homework finished

--- app/controllers/guns_controller.rb
+++ app/controllers/guns_controller.rb
@@ -1,13 +1,20 @@
 class GunsController < ApplicationController
-	before_action :set_gun, :only => [ :show, :edit, :update, :destroy]
+	before_action :set_gun, :only => [ :show, :update, :destroy]
 
   def index
 		  @guns = Gun.page(params[:page]).per(10)
+
+      if params[:mgun]
+         @gun = Gun.find(params[:mgun])
+      else
+         @gun = Gun.new
+      end
+
 	end
 
-	def new
-		  @gun = Gun.new
-	end	
+	# def new
+	# 	  @gun = Gun.new
+	# end	
 
 	def create
 	   	 @gun= Gun.new(gun_params)
@@ -19,26 +26,27 @@ class GunsController < ApplicationController
     end 
   end
 
-  def edit
-  end	
+  # def edit
+  # end	
 
   def show
       @page_title = @gun.name
   end
 
   def update
-    	if @gun.update(gun_params)
+    	# if
+         @gun.update(gun_params)
          flash[:notice]="更新成功！"
          redirect_to gun_path(@gun)
-      else
-      	 render :action => :edit  
-      end
+      # else
+      # 	 render :action => :edit  
+      #end
   end
   
   def destroy
         @gun.destroy
         flash[:alert] = "刪除成功！"     
-      	redirect_to :back
+      	redirect_to guns_path(params[:page])
   end
 
 private
